23e3c8fc929d05757f091dbd2e7810210263a36d
hagigat <hagigat.sajjad.3@gmail.com>
2019-07-31 12:41:45 +0430

    BE-AddApiControllerVisit-v1.1

---
+++ Clinic/Clinic/Controllers/DoctorsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Clinic.Models;
+
+namespace Clinic.Controllers
+{
+    public class DoctorsController : ApiController
+    {
+        private DB db = new DB();
+
+        // GET: api/Doctors
+        public IQueryable<Doctor> GetDoctors()
+        {
+            return db.Doctors;
+        }
+
+        // GET: api/Doctors/5
+        [ResponseType(typeof(Doctor))]
+        public IHttpActionResult GetDoctor(string id)
+        {
+            Doctor doctor = db.Doctors.Find(id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(doctor);
+        }
+
+        // PUT: api/Doctors/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDoctor(string id, Doctor doctor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != doctor.MCC)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(doctor).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DoctorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Doctors
+        [ResponseType(typeof(Doctor))]
+        public IHttpActionResult PostDoctor(Doctor doctor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Doctors.Add(doctor);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (DoctorExists(doctor.MCC))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = doctor.MCC }, doctor);
+        }
+
+        // DELETE: api/Doctors/5
+        [ResponseType(typeof(Doctor))]
+        public IHttpActionResult DeleteDoctor(string id)
+        {
+            Doctor doctor = db.Doctors.Find(id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            db.Doctors.Remove(doctor);
+            db.SaveChanges();
+
+            return Ok(doctor);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DoctorExists(string id)
+        {
+            return db.Doctors.Count(e => e.MCC == id) > 0;
+        }
+    }
+}
---
+++ Clinic/Clinic/Controllers/UsersController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text;
+using System.Web.Hosting;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Clinic.Models;
+
+namespace Clinic.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private DB db = new DB();
+
+        // GET: api/Users
+        public IQueryable<User> GetUsers()
+        {
+            return db.Users;
+        }
+
+        // GET: api/Users/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult GetUser(string id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/Users/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUser(string id, User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.UNcode)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Users
+        [ResponseType(typeof(User))]
+        public IHttpActionResult PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            user.IsValid = false;
+            var random = Convert.ToBase64String(Guid.NewGuid().ToByteArray()) + DateTime.Now.Ticks;
+            random = System.Text.RegularExpressions.Regex.Replace(random, "[^0-9a-zA-Z]+", "");
+            user.Token = random;
+
+            db.Users.Add(user);
+
+            try
+            {
+                db.SaveChanges();
+                if (user.IsValid == false)
+                {
+                    string URL = "http://localhost:4200/#/active-account/" + user.Token;
+                    string body = System.IO.File.ReadAllText(HostingEnvironment.MapPath("~/EmailTemplate/") + "Text" + ".cshtml");
+                    body = body.Replace("@URL", URL);
+                    SendEmail(user.Email, "کلینیک خاص - فعال سازی حساب کاربری", body);
+                }
+            }
+            catch (DbUpdateException)
+            {
+                if (UserExists(user.UNcode))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = user.UNcode }, user);
+        }
+
+        private void SendEmail(string toEmail, string subject, string bodyEmail)
+        {
+            try
+            {
+                string senderEmail = System.Configuration.ConfigurationManager.AppSettings["SenderEmail"].ToString();
+                string senderPassword = System.Configuration.ConfigurationManager.AppSettings["SenderPassword"].ToString();
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.Timeout = 100000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(senderEmail, senderPassword);
+
+                MailMessage mail = new MailMessage(senderEmail, toEmail, subject, bodyEmail);
+                mail.IsBodyHtml = true;
+                mail.BodyEncoding = UTF8Encoding.UTF8;
+                client.Send(mail);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        // DELETE: api/Users/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult DeleteUser(string id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserExists(string id)
+        {
+            return db.Users.Count(e => e.UNcode == id) > 0;
+        }
+    }
+}
