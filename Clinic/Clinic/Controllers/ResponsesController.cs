a588761d78ae78d066c623bcb45d5d46ea637edb
hagigat <hagigat.sajjad.3@gmail.com>
2019-08-06 16:42:45 +0430

    BE-AddPostController-v1.4

---
+++ Clinic/Clinic/Controllers/ResponsesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Clinic.Models;
+
+namespace Clinic.Controllers
+{
+    public class ResponsesController : ApiController
+    {
+        private DB db = new DB();
+
+        // GET: api/Responses
+        public IQueryable<Response> GetResponses()
+        {
+            return db.Responses;
+        }
+
+        // GET: api/Responses/5
+        [ResponseType(typeof(Response))]
+        public IHttpActionResult GetResponse(int id)
+        {
+            Response response = db.Responses.Find(id);
+            if (response == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(response);
+        }
+
+        // PUT: api/Responses/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutResponse(int id, Response response)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != response.RID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(response).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ResponseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Responses
+        [ResponseType(typeof(Response))]
+        public IHttpActionResult PostResponse(Response response)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Responses.Add(response);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = response.RID }, response);
+        }
+
+        // DELETE: api/Responses/5
+        [ResponseType(typeof(Response))]
+        public IHttpActionResult DeleteResponse(int id)
+        {
+            Response response = db.Responses.Find(id);
+            if (response == null)
+            {
+                return NotFound();
+            }
+
+            db.Responses.Remove(response);
+            db.SaveChanges();
+
+            return Ok(response);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ResponseExists(int id)
+        {
+            return db.Responses.Count(e => e.RID == id) > 0;
+        }
+    }
+}
